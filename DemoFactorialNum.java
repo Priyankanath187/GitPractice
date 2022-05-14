91668f9fb84124d3cfb6e2a6d5b3bc1360b0ccb0
unknown <priyankanath187@gmail.com>
2022-05-14 08:30:15 +0530

    modified files as part ofCSL-12

--- DemoFactorialNum.java
+++ DemoFactorialNum.java
@@ -3,7 +3,7 @@ class FactorialExample{
     //main() method starts   
     public static void main(String args[]){    
         int i,factorial=1;    
-  
+  System.out.println("I am from Factorial logic"); 
         int n = 8;  // Number whose factorial we want to calculate  
         //Calculating factorial of number 8  
         for(i=1;i<=n;i++){      
--- TestFibonacciSeries.java
+++ TestFibonacciSeries.java
@@ -3,13 +3,13 @@ class FactorialExample{
     //main() method starts   
     public static void main(String args[]){    
         int i,factorial=1;    
-  
+  System.out.println("I am from fibonacci logic body"); 
         int n = 8;  // Number whose factorial we want to calculate  
         //Calculating factorial of number 8  
         for(i=1;i<=n;i++){      
             factorial = factorial*i;      
-        }  
+        }  System.out.println(factorial+" is the factorial of: "+n); 
         // Showing factorial of the number      
-        System.out.println(factorial+" is the factorial of: "+n);      
+             
     }    
 }  
